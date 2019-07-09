9301eb45faf98f96536bf128fdcdcbdf3ebbeaf4
Vikil Naik <vikilnaik37@gmail.com>
2019-07-09 17:34:37 -0400

    added scroll animation, fixed up the featured page, still have to style the barber and chef page

--- main.js
+++ main.js
@@ -12,3 +12,52 @@ function showSlides() {
     slides[slideIndex - 1].style.display = "block"
     setTimeout(showSlides, 5000); 
 }
+
+const navbarToggler = document.querySelector(".navbar-toggle");
+const navbarMenu = document.querySelector(".main-nav");
+const navbarLinks = document.querySelectorAll(".main-nav a");
+
+navbarToggler.addEventListener("click", navbarTogglerClick);
+
+function navbarTogglerClick() {
+    navbarToggler.classList.toggle("open-navbar-toggle");
+    navbarMenu.classList.toggle("open");
+}
+
+navbarLinks.forEach(elem => elem.addEventListener("click", navbarLinkClick));
+
+function navbarLinkClick(event) {
+    smoothScroll(event);
+    if (navbarMenu.classList.contains("open")) {
+        navbarToggler.click();
+    }
+}
+
+function smoothScroll(event) {
+    event.preventDefault();
+    const targetId = event.currentTarget.getAttribute("href");
+    const targetPosition = document.querySelector(targetId).offsetTop;
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    const duration = 2000;
+    let start = null;
+
+    window.requestAnimationFrame(step);
+
+    function step(timestamp) {
+        if (!start) start = timestamp;
+        const progress = timestamp - start;
+        window.scrollTo(
+            0,
+            easeInOutCubic(progress, startPosition, distance, duration)
+        );
+        if (progress < duration) window.requestAnimationFrame(step);
+    }
+}
+
+function easeInOutCubic(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t * t + b;
+    t -= 2;
+    return (c / 2) * (t * t * t + 2) + b;
+}
